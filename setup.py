c127dcfe64b9dc804fc21bd675d79fb7e62118b9
Christian Brodbeck <christianmbrodbeck@gmail.com>
2019-11-21 11:56:36 -0500

    Version 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TextGrid',
-    version='1.4',
+    version='1.5',
     author='Kyle Gorman et al.',
     author_email='kylebgorman@gmail.com',
     packages=['textgrid'],
